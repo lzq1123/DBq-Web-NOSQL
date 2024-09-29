4fa00fd2326cee4a0d868cc8109082adf4d89fbc
W3iQiang <2303095@sit.singaporetech.edu.sg>
2024-09-29 16:04:15 +0800

    Setting up

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     # Configuration for PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/TicketGrabdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:19V28ydb@localhost/TicketGrabdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
