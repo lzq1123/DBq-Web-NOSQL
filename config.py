d651d2e816aba4735010e88ab03945a42147ffd0
W3iQiang <2303095@sit.singaporetech.edu.sg>
2024-09-29 16:21:11 +0800

    strawberry

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     # Configuration for PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:19V28ydb@localhost/TicketGrabdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://DBProjGrp38:strawberries@ticketgrabdb.cugroa0wbny6.us-east-1.rds.amazonaws.com:5432/TicketGrabdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
